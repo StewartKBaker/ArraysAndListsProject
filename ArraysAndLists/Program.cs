ac4f6cffed95c8069d9e421fc1bc2cb6e8634dc6
StewartBaker <bakrstewart16@gmail.com>
2024-07-15 11:31:59 -0500

    finished exercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -7,38 +8,54 @@ namespace ArraysAndLists
         static void Main(string[] args)
         {
             //TODO:
-
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             // Create an int array and populate it with numbers 1-10
-            
+
 
             /* Create a list of type int
              * Name the list "evens"
              */
+            var evens = new List<int>();
 
-            
             /* Create another list of type int
              * Name the list "odds"
              */
-            
+            var odds = new List<int>();
 
-             /* Using either a foreach or for loop,
+            /* Using either a foreach or for loop,
              * iterate through the array you populated with 10 numbers.
              * Inside the scope of the loop,
              * check to see if each number in the array is even or odd.
              * If the number is even, add it to the evens list.
              * If the number is odd, add it to the odds list.
              */
-
+            foreach (var num in numbers)
+            {
+             if (num % 2 == 0)
+              {
+               evens.Add(num);
+              }
+              else odds.Add(num);
+            }
 
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
-            
+            Console.WriteLine("Evens:");
+            foreach (var num in evens)
+            {
+             Console.WriteLine(num);
+            }
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+            Console.WriteLine("\nOdds:");
+            foreach (var num in odds)
+            {
+             Console.WriteLine(num);
+            }
         }
     }
 }
